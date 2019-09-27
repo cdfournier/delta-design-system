e4e717e4875328b73745443f28d0776ac69ade81
Chris <chris.d.fournier@gmail.com>
2019-09-27 19:45:07 -0400

    Started form-atoms page and other updates
    Added script for show/hide password; simplified Blockquote page layout; updated footer border width.

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,3 +1,13 @@
 $('button.menu-toggle').click(function() {
   $('nav').toggleClass('open');
 });
+
+$(".show-hide-password").click(function() {
+  $(this).toggleClass("show");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
