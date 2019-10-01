3435b08fd8175561e7e2263290b347c3e72bfee3
Chris <chris.d.fournier@gmail.com>
2019-10-01 19:00:39 -0400

    Alerts page added
    Also includes assorted css updates

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -11,3 +11,29 @@ $(".show-hide-password").click(function() {
     input.attr("type", "password");
   }
 });
+
+$('.show-error').click( function() {
+  $('.alerts .error').addClass('show');
+  setTimeout(function() { 
+    $('.alerts .error').removeClass('show') }, 10000);
+});
+$('svg.close').click( function() {
+  $('.alerts .error').removeClass('show');
+});
+$('.show-success').click( function() {
+  $('.alerts .success').addClass('show');
+  setTimeout(function() { 
+    $('.alerts .success').removeClass('show') }, 10000);
+});
+$('svg.close').click( function() {
+  $('.alerts .success').removeClass('show');
+});
+$('.show-confirm').click( function() {
+  $('.alerts .confirm').addClass('show');
+});
+$('button.no').click( function() {
+  $('.alerts .confirm').removeClass('show');
+});
+$('button.yes').click( function() {
+  $('.alerts .confirm').removeClass('show');
+});
