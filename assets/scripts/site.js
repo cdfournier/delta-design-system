c96898165f3b5e27a04b841c2a77c20b069503c9
Chris <chris.d.fournier@gmail.com>
2020-01-05 14:24:10 -0500

    Updated demo
    This one looks better to me.

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,17 +1,17 @@
 // MOBILE NAVIGATION TOGGLES
 $('button.menu-toggle svg.menu-toggle-open').click(function() {
-  $('header').addClass('open');
   $('nav').addClass('open');
   $('body').addClass('open');
   $('header .logo').addClass('open');
+  $('.banner:first-of-type').addClass('open');
   $('button.menu-toggle').attr('aria-expanded',true);
   $('nav ul').attr('aria-hidden',false);
 });
 $('button.menu-toggle svg.menu-toggle-close').click(function() {
-  $('header').removeClass('open');
   $('nav').removeClass('open');
   $('body').removeClass('open');
   $('header .logo').removeClass('open');
+  $('.banner:first-of-type').removeClass('open');
   $('button.menu-toggle').attr('aria-expanded',false);
   $('nav ul').attr('aria-hidden',true);
 });
