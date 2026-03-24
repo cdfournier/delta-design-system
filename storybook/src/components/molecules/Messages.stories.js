b1d32d63060d379aa16a17f03ffde906d61fc01a
Chris <chris.d.fournier@gmail.com>
2026-03-24 11:46:35 -0400

    fix(molecules): Messages close button sizing and hover states

--- storybook/src/components/molecules/Messages.stories.js
+++ storybook/src/components/molecules/Messages.stories.js
@@ -1,5 +1,5 @@
 const closeIcon = `
-  <svg viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <svg viewBox="0 0 24 24" style="width: 100%; height: 100%;" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
   </svg>
